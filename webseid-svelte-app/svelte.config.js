b00e23c4d46273aa9090baa07fee63eaa8365982
Dustin Seidler <seidler.dustin@web.de>
2023-02-09 19:39:45 +0100

    svelte config revert

--- webseid-svelte-app/svelte.config.js
+++ webseid-svelte-app/svelte.config.js
@@ -2,14 +2,6 @@ import adapter from '@sveltejs/adapter-node';
  
 export default {
   kit: {
-    csp: {
-      directives: {
-        'script-src': ['self']
-      },
-      reportOnly: {
-        'script-src': ['self']
-      }
-    },
 
     adapter: adapter()
   
