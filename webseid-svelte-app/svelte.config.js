2bf465c2c2f0f38fe13dfa994e00673242c18650
Dustin Seidler <seidler.dustin@web.de>
2023-02-09 14:00:18 +0100

    content security policy

--- webseid-svelte-app/svelte.config.js
+++ webseid-svelte-app/svelte.config.js
@@ -2,6 +2,16 @@ import adapter from '@sveltejs/adapter-node';
  
 export default {
   kit: {
+    csp: {
+      directives: {
+        'script-src': ['self']
+      },
+      reportOnly: {
+        'script-src': ['self']
+      }
+    },
+
     adapter: adapter()
+  
   }
 };
